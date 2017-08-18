c432ccaf8efc4cfeef9b915b69212e374bdd807f
Daniel Hoherd <daniel.hoherd@gmail.com>
2017-08-18 21:05:27 +0700

    Cleanup

--- dayone_2_to_1/cli.py
+++ dayone_2_to_1/cli.py
@@ -4,13 +4,10 @@ from DayoneConverter import DayoneConverter
 @click.command()
 @click.argument('journal', default='Journal.json', required=False)
 def main(journal):
-    """Converts DayOne2 journal.json to DayOne Classic xml"""
-    click.echo("working with {}".format(journal))
+    """Converts DayOne2 journal.json to DayOne Classic xml files"""
+
     j = DayoneConverter()
     j.load_journal(journal)
-    # for entry in j:
-    #     print(entry)
-    # j.dump_journal()
     j.dump_plists()
 
 

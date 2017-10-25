af0bd05e99f79ee79ef1d37976df3cd74c6e013d
Daniel Hoherd <daniel.hoherd@gmail.com>
2017-10-25 16:34:46 -0700

    Add argument validation, .flake8

--- dayone_2_to_1/cli.py
+++ dayone_2_to_1/cli.py
@@ -1,13 +1,24 @@
+# -*- coding: utf-8 -*-
 import click
+import os
 from dayone_2_to_1 import DayoneConverter
 
+
+def validate_filename(ctx, opts, args):
+    try:
+        assert os.path.isfile(args)
+        return args
+    except AssertionError:
+        raise click.BadParameter('File "{}" does not exist! '
+                                 'Must be a valid Journal.json from DayOne 2 export.'.format(args))
+
+
 @click.command()
-@click.argument('journal', default='Journal.json', required=False)
+@click.argument('journal', default='Journal.json', required=False,
+                callback=validate_filename, nargs=1)
 def main(journal):
-    """Converts DayOne2 journal.json to DayOne Classic xml files"""
+    """Converts DayOne2 Journal.json to DayOne Classic xml files"""
 
     j = DayoneConverter()
     j.load_journal(journal)
     j.dump_plists()
-
-
